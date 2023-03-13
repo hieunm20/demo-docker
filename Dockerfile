49013916d127a9206ca683abb255b8f1beab8933
hieunm <minhhieuittlu@gmail.com>
2023-03-13 10:31:40 +0700

    no message

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,5 @@
-FROM C:\Program Files\Java\jdk-11.0.16.1
+FROM adoptopenjdk/openjdk11:latest
 MAINTAINER com.example
-RUN addgroup --system spring && adduser -S -s /bin/false -G spring spring
 USER spring
 COPY target/demo-docker-0.0.1-SNAPSHOT.jar app.jar
 ENTRYPOINT ["java","-jar","/app.jar"]
--- src/main/java/com/example/demo/docker/DemoDockerApplication.java
+++ src/main/java/com/example/demo/docker/DemoDockerApplication.java
@@ -1,13 +1,26 @@
 package com.example.demo.docker;
 
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
-public class DemoDockerApplication {
+public class DemoDockerApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoDockerApplication.class, args);
-	}
+    @Value("${server.port}")
+    private int serverPort;
 
+    public static void main(String[] args) {
+        SpringApplication.run(DemoDockerApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        log.info("(run) server running in {}", serverPort);
+        log.info("(run) is empty {}", StringUtils.isEmpty("Xin chao"));
+    }
 }
